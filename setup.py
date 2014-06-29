80da20203e3368ba98f21c17980b3112a056ce1a
Sascha Häusler <saeschdivara@gmail.com>
2014-06-29 14:39:32 +0200

    This is now a package on https://pypi.python.org

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ArangoPy',
+    version='0.0.1',
+    packages=['slumber', 'arangodb'],
+    url='https://github.com/saeschdivara/ArangoPy',
+    license='MIT',
+    author='saskyrardisaskyr',
+    author_email='saeschdivara@gmail.com',
+    description=''
+)
