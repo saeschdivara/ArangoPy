de5fe05ce77103e57ecc7014075be29f13bc0f2a
Sascha Haeusler <sascha.haeusler@netzbarkeit.ch>
2014-07-08 08:21:40 +0200

    Added testing file

---
+++ arangodb/tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class AqlQueryTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_this(self):  ## test method names begin 'test*'
+        self.assertEqual((1 + 2), 3)
+        self.assertEqual(0 + 1, 1)
+
+    def testMultiply(self):
+        self.assertEqual((0 * 10), 0)
+        self.assertEqual((5 * 8), 40)
+
+if __name__ == '__main__':
+    unittest.main()
