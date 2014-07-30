7c1299d8d6fe6461e096eaf607f34b3eb7791d59
Sascha Haeusler <sascha.haeusler@netzbarkeit.ch>
2014-07-30 08:12:54 +0200

    Moving fields to other file

--- arangodb/models.py
+++ arangodb/models.py
@@ -1,60 +1,6 @@
 from arangodb.api import Collection
+from arangodb.fields import ModelField
 
-
-class ModelField(object):
-
-    def __init__(self, is_required=True, **kwargs):
-        """
-        """
-
-        self.is_required = is_required
-
-    def dumps(self):
-        """
-        """
-
-        return u''
-
-    def loads(self, string_val):
-        """
-        """
-
-        pass
-
-    def set(self, *args, **kwargs):
-        """
-        """
-
-        pass
-
-class TextField(ModelField):
-
-    def __init__(self, is_required=True, **kwargs):
-        """
-        """
-
-        super(TextField, self).__init__(**kwargs)
-
-        self.text = u''
-
-    def dumps(self):
-        """
-        """
-
-        return u'%s' % self.text
-
-    def loads(self, string_val):
-        """
-        """
-
-        self.text = string_val
-
-    def set(self, *args, **kwargs):
-        """
-        """
-
-        if len(args) is 1:
-            self.text = args[0]
 
 class CollectionModel(object):
 
