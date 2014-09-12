adf0b9db3c787c210ea1e96239d9f2cb8677468c
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-09-12 15:41:04 +0200

    Made general simple query construction possible

--- arangodb/query/simple.py
+++ arangodb/query/simple.py
@@ -5,28 +5,22 @@ from query.utils.document import create_document_from_result_dict
 
 
 class SimpleQuery(object):
+    """
+    """
+
+
+    @classmethod
+    def all(cls, collection):
+        """
+        """
+
+
     @classmethod
     def getByExample(cls, collection, example_data):
         """
         """
 
-        query = {
-            'collection': collection.name,
-            'example': example_data,
-        }
-
-        client = Client.instance()
-        client.set_database(collection.database)
-        api = client.api
-        result_dict = api.simple('by-example').put(data=query)
-
-        if result_dict['count'] == 0:
-            return None
-
-        try:
-            return create_document_from_result_dict(result_dict['result'][0], api)
-        except:
-            return result_dict
+        return SimpleQuery._construct_query(name='by-example', collection=collection, example=example_data)
 
 
     @classmethod
@@ -34,12 +28,26 @@ class SimpleQuery(object):
         """
         """
 
+        return SimpleQuery._construct_query(name='any', collection=collection)
+
+
+    @classmethod
+    def _construct_query(cls, name, collection, **kwargs):
+        """
+        """
+
         query = {
-            'collection': collection,
+            'collection': collection.name,
         }
 
-        api = Client.instance().api
-        result_dict = api.simple('any').put(data=query)
+        for arg_name in kwargs:
+            query[arg_name] = kwargs[arg_name]
+
+
+        client = Client.instance()
+        client.set_database(collection.database)
+        api = client.api
+        result_dict = api.simple(name).put(data=query)
 
         if result_dict['count'] == 0:
             return None
