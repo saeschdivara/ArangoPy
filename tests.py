7885e07160b99b9cf8a530cc6254ef8d447b80e8
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-09-12 09:56:17 +0200

    Added all test suites for ci

--- tests.py
+++ tests.py
@@ -2,7 +2,8 @@
 import unittest
 
 # Test suites
-from arangodb.tests import DatabaseTestCase, CollectionTestCase, AqlQueryTestCase
+from arangodb.tests import DatabaseTestCase, CollectionTestCase, AqlQueryTestCase, DocumentTestCase, SimpleQueryTestCase, \
+    CollectionModelTestCase, CollectionModelForeignKeyFieldTestCase
 
 
 
@@ -15,7 +16,11 @@ failures = 0
 # Test suites
 test_suites.append( unittest.TestLoader().loadTestsFromTestCase(DatabaseTestCase) )
 test_suites.append( unittest.TestLoader().loadTestsFromTestCase(CollectionTestCase) )
+test_suites.append( unittest.TestLoader().loadTestsFromTestCase(DocumentTestCase) )
 test_suites.append( unittest.TestLoader().loadTestsFromTestCase(AqlQueryTestCase) )
+test_suites.append( unittest.TestLoader().loadTestsFromTestCase(SimpleQueryTestCase) )
+test_suites.append( unittest.TestLoader().loadTestsFromTestCase(CollectionModelTestCase) )
+test_suites.append( unittest.TestLoader().loadTestsFromTestCase(CollectionModelForeignKeyFieldTestCase) )
 
 for test_suite in test_suites:
 
