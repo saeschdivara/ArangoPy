7992e112e1188d15b084631d333df57e0aebff43
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-09-18 07:35:21 +0200

    Added update action

--- arangodb/transaction/action.py
+++ arangodb/transaction/action.py
@@ -10,7 +10,7 @@ class DocumentAction(TransactionAction):
     """
     """
 
-    def __init__(self, action_type, collection_name, document_data):
+    def __init__(self, action_type, collection_name, document_data, _id=''):
         """
         """
 
@@ -18,6 +18,7 @@ class DocumentAction(TransactionAction):
 
         self.collection_name = collection_name
         self.document_data = document_data
+        self._id = _id
 
     @classmethod
     def create(cls, collection_name, document_data):
@@ -25,3 +26,10 @@ class DocumentAction(TransactionAction):
         """
 
         return cls(action_type='create', collection_name=collection_name, document_data=document_data)
+
+    @classmethod
+    def update(cls, collection_name, _id, document_data):
+        """
+        """
+
+        return cls(action_type='update', collection_name=collection_name, document_data=document_data, _id=_id)
