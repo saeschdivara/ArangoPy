58cbc53a56f61f30544465f04228fbeee28c9575
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-09-24 11:32:44 +0200

    Include all packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ArangoPy',
     version='0.1.1',
-    packages=['arangodb'],
+    packages=find_packages(),
     requires=[
         'slumber',
     ],
