a5ee0f20d1691a84731d20f0b900051bdafce04b
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-10-14 07:26:26 +0200

    Updated general index documentation

--- arangodb/index/general.py
+++ arangodb/index/general.py
@@ -1,6 +1,8 @@
 
 class BaseIndex(object):
     """
+        This is the base class for all other index
+        but which has no functionality
     """
 
     type_name = None
@@ -14,17 +16,30 @@ class BaseIndex(object):
 
     def get_extra_attributes(self):
         """
+            You need to override this method for each index
+            so it can return a dict with all values which are needed
+            for the index
         """
 
 
 class FulltextIndex(BaseIndex):
     """
+        Index for fulltext search
     """
 
     type_name = 'fulltext'
 
     def __init__(self, fields, minimum_length):
         """
+            Creates fulltext index which is configured for the fields with
+            the minimum word length
+
+            :param fields A list of attribute names. Currently, the list is limited to exactly one attribute, so the
+            value of fields should look like this for example: [ "text" ].
+
+            :param minimum_length Minimum character length of words to index. Will default to a server-defined value
+            if unspecified. It is thus recommended to set this value explicitly when creating the index.
+
         """
 
         super(FulltextIndex, self).__init__()
@@ -43,11 +58,22 @@ class FulltextIndex(BaseIndex):
 
 
 class CapConstraintIndex(BaseIndex):
+    """
+        The cap constraint does not index particular attributes of the documents in a collection,
+        but limits the number of documents in the collection to a maximum value.
+        The cap constraint thus does not support attribute names specified in the fields attribute
+        nor uniqueness of any kind via the unique attribute.
+    """
 
     type_name = 'cap'
 
     def __init__(self, size, document_byte_size=16384):
         """
+            :param size  The maximal number of documents for the collection. If specified, the value
+            must be greater than zero.
+
+            :param document_byte_size The maximal size of the active document data in the collection (in bytes).
+            If specified, the value must be at least 16384.
         """
 
         super(CapConstraintIndex, self).__init__()
