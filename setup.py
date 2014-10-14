87079491eaa5f1c00501b26b295aefde155178e5
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-10-14 19:08:36 +0200

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ArangoPy',
-    version= '0.3.2',
+    version= '0.4',
     packages=find_packages(),
     requires=[
         'slumber',
