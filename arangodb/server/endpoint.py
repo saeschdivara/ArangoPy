8871c5912921b931650cf5fcdda094d11ed4782f
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-10-16 21:56:20 +0200

    Creating works but delete doesn't

--- arangodb/server/endpoint.py
+++ arangodb/server/endpoint.py
@@ -30,3 +30,51 @@ class Endpoint(object):
         endpoint_list = api.endpoint.get()
 
         return endpoint_list
+
+    @classmethod
+    def create(cls, url, databases):
+        """
+            If databases is an empty list, all databases present in the server will become accessible via the endpoint,
+            with the _system database being the default database.
+
+            If databases is non-empty, only the specified databases will become available via the endpoint.
+            The first database name in the databases list will also become the default database for the endpoint.
+            The default database will always be used if a request coming in on the endpoint does not specify
+            the database name explicitly.
+
+            *Note*: adding or reconfiguring endpoints is allowed in the system database only.
+            Calling this action in any other database will make the server return an error.
+
+            Adding SSL endpoints at runtime is only supported if the server was started with SSL
+            properly configured (e.g. --server.keyfile must have been set).
+
+            :param url the endpoint specification, e.g. tcp://127.0.0.1:8530
+            :param databases a list of database names the endpoint is responsible for.
+        """
+
+        api = Client.instance().api
+
+        result = api.endpoint.post(data={
+            'endpoint': url,
+            'databases': databases,
+        })
+
+        return result
+
+    @classmethod
+    def destroy(cls, url):
+        """
+            This operation deletes an existing endpoint from the list of all endpoints,
+            and makes the server stop listening on the endpoint.
+
+            *Note*: deleting and disconnecting an endpoint is allowed in the system database only.
+            Calling this action in any other database will make the server return an error.
+
+            Futhermore, the last remaining endpoint cannot be deleted as this would make the server kaput.
+
+            :param url The endpoint to delete, e.g. tcp://127.0.0.1:8529.
+        """
+
+        api = Client.instance().api
+
+        api.endpoint(url).delete()
--- arangodb/tests.py
+++ arangodb/tests.py
@@ -1560,8 +1560,24 @@ class EndpointTestCase(unittest.TestCase):
 
         endpoints = Endpoint.all()
 
-        self.assertTrue('endpoint' in endpoints)
-        self.assertTrue('databases' in endpoints)
+        self.assertTrue(len(endpoints) > 0)
+
+        for endpoint in endpoints:
+            self.assertTrue('endpoint' in endpoint)
+            self.assertTrue('databases' in endpoint)
+
+    def test_create_and_delete_endpoint(self):
+
+        endpoint_url = 'tcp://127.0.0.1:2211'
+
+        Endpoint.create(url=endpoint_url, databases=[])
+
+        endpoints = Endpoint.all()
+
+        self.assertTrue(len(endpoints) > 1)
+        print(endpoints)
+
+        Endpoint.destroy(url=endpoint_url)
 
 if __name__ == '__main__':
     unittest.main()
