5aac499667b9496b39b64fb6878769e8205534f4
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-11-04 17:46:26 +0100

    New version with some filtering options added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ArangoPy',
-    version= '0.4.4.1',
+    version= '0.4.4.2',
     packages=find_packages(),
     requires=[
         'slumber',
