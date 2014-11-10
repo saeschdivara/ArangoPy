7c26f872384c5ebe4d10e9617577efd23cbd9446
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-11-10 17:14:04 +0100

    Adding contains and icontains and a list-field for model collections

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ArangoPy',
-    version= '0.4.4.2',
+    version= '0.4.5',
     packages=find_packages(),
     requires=[
         'slumber',
