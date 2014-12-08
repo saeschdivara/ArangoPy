d703e230b3f717dfc1e9ce85f0b236971f391d11
Sascha <saskyrar@Saschas-MacBook-Pro.local>
2014-12-08 09:34:05 +0100

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ArangoPy',
-    version= '0.5.2',
+    version= '0.5.3',
     packages=find_packages(),
     requires=[
         'slumber',
