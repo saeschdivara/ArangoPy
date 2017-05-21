b622cc336c198b27d5577c058ef16ea6ffb9731a
Python3pkg <raliclo@gmail.com>
2017-05-21 00:52:52 -0700

    "Update Travis-CI arangodb link"
    "https://www.arangodb.com/repositories/travisCI/setup_arangodb_3.2.alpha4.sh"

--- scripts/ArangoDB/arangodb_startup.sh
+++ scripts/ArangoDB/arangodb_startup.sh
@@ -1,67 +1,67 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  cd $DIR
+cd $DIR
 
-  VERSION=3.2.alpha4
-  NAME=ArangoDB-$VERSION
+VERSION=3.2.alpha4
+NAME=ArangoDB-$VERSION
 
-  if [ ! -d "$DIR/$NAME" ]; then
-    # download ArangoDB
-    echo "wget --no-check-certificate http://www.arangodb.com/repositories/Source/$NAME.tar.gz"
-    wget http://www.arangodb.org/travisCI/$NAME.tar.gz
-    echo "tar zxf $NAME.tar.gz"
-    tar zvxf $NAME.tar.gz
-  fi
+if [ ! -d "$DIR/$NAME" ]; then
+  # download ArangoDB
+  echo "curl -L -o $NAME.tar.gz https://www.arangodb.org/repositories/travisCI/$NAME.tar.gz"
+  curl -L -o $NAME.tar.gz https://www.arangodb.org/repositories/travisCI/$NAME.tar.gz
+  echo "tar zxf $NAME.tar.gz"
+  tar zvxf $NAME.tar.gz
+fi
 
-  ARCH=$(arch)
-  PID=$(echo $PPID)
-  TMP_DIR="/tmp/arangodb.$PID"
-  PID_FILE="/tmp/arangodb.$PID.pid"
-  ARANGODB_DIR="$DIR/$NAME"
+ARCH=$(arch)
+PID=$(echo $PPID)
+TMP_DIR="/tmp/arangodb.$PID"
+PID_FILE="/tmp/arangodb.$PID.pid"
+ARANGODB_DIR="$DIR/$NAME"
+ARANGOD="${ARANGODB_DIR}/bin/arangod_x86_64"
 
-  ARANGOD="${ARANGODB_DIR}/bin/arangod"
+# create database directory
+mkdir ${TMP_DIR}
 
-  if [ "$ARCH" -eq "x86_64" ]; then
-  ARANGOD="${ARANGOD}_x86_64"
-  fi
+echo "Starting ArangoDB '${ARANGOD}'"
 
-  # create database directory
-  mkdir ${TMP_DIR}
+${ARANGOD} \
+    --database.directory ${TMP_DIR} \
+    --configuration none \
+    --server.endpoint tcp://127.0.0.1:8529 \
+    --javascript.app-path ${ARANGODB_DIR}/js/apps \
+    --javascript.startup-directory ${ARANGODB_DIR}/js \
+    --database.maximal-journal-size 1048576 \
+    --server.authentication false &
 
-  echo "Starting ArangoDB '${ARANGOD}'"
+sleep 2
 
-  ${ARANGOD} \
-      --database.directory ${TMP_DIR}  \
-      --configuration none  \
-      --server.endpoint tcp://127.0.0.1:8529 \
-      --javascript.startup-directory ${ARANGODB_DIR}/js \
-      --server.admin-directory ${ARANGODB_DIR}/html/admin \
-      --javascript.modules-path ${ARANGODB_DIR}/js/server/modules:${ARANGODB_DIR}/js/common/modules:${ARANGODB_DIR}/js/node \
-      --javascript.package-path ${ARANGODB_DIR}/js/npm:${ARANGODB_DIR}/js/common/test-data/modules \
-      --javascript.app-path ${ARANGODB_DIR}/js/apps \
-      --javascript.action-directory ${ARANGODB_DIR}/js/actions \
-      --database.maximal-journal-size 1048576  \
-      --server.disable-admin-interface true \
-      --server.disable-authentication true \
-      --javascript.gc-interval 1 &
+echo "Check for arangod process"
+process=$(ps auxww | grep "bin/arangod" | grep -v grep)
 
-  sleep 2
+if [ "x$process" == "x" ]; then
+  echo "no 'arangod' process found"
+  echo "ARCH = $ARCH"
+  exit 1
+fi
 
-  echo "Check for arangod process"
-  process=$(ps auxww | grep "bin/arangod" | grep -v grep)
+echo "Waiting until ArangoDB is ready on port 8529"
 
-  if [ "$process" == "x" ]; then
-    echo "no 'arangod' process found"
-    echo "ARCH = $ARCH"
+n=0
+# timeout value for startup
+timeout=60 
+while [[ (-z `curl -H 'Authorization: Basic cm9vdDo=' -s 'http://127.0.0.1:8529/_api/version' `) && (n -lt timeout) ]] ; do
+  echo -n "."
+  sleep 1s
+  n=$[$n+1]
+done
+
+if [[ n -eq timeout ]];
+then
+    echo "Could not start ArangoDB. Timeout reached."
     exit 1
-  fi
+fi
 
-  echo "Waiting until ArangoDB is ready on port 8529"
-  while [ -z `curl -s 'http://127.0.0.1:8529/_api/version' ` ]
-  do
-    echo -n "."
-    sleep 2s
-  done
 
-  echo "ArangoDB is up"
+echo "ArangoDB is up"
